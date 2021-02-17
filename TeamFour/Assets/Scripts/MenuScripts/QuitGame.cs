7eaef75a01bd8d140329735fee5773da1d4c7102
faeythfreedom <faithbrownlee@hotmail.co.uk>
2021-02-17 11:28:43 +0000

    Update 5 Tilesets

---
+++ TeamFour/Assets/Scripts/MenuScripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
---
+++ TeamFour/Assets/Scripts/MenuScripts/SelectForNavigation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectForNavigation : MonoBehaviour
+{
+    public bool selectOnActivate = true;
+
+    void OnEnable()
+    {
+        if (selectOnActivate) Invoke("Select", 0.1f);
+    }
+
+    public void Select()
+    {
+        GetComponent<Selectable>().Select();
+    }
+}
